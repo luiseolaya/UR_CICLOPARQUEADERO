085afe4471f1da298afb8d3c9469141973c8fc04
luiseolaya <luisolaya2011@gmail.com>
2025-03-17 10:51:51 -0500

    correcion

--- app/init.php
+++ app/init.php
@@ -5,4 +5,3 @@ require_once __DIR__ . '../controllers/LogoutController.php';
 require_once __DIR__ . '../controllers/UsuarioController.php';
 require_once __DIR__ . '/models/Usuario.php';
 require_once __DIR__ . '/models/Entrada.php';
-require_once __DIR__ . '/models/ModeloEvidencia.php';;
